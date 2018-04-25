4720f709c866e410ab10eb5d320b10fc2445e78f
Ru3HaM <34649038+Ru3HaM@users.noreply.github.com>
2018-04-26 00:13:26 +0300

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+	$( function() {
+    	$( "#datepicker" ).datepicker();
+  	});
+	   $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+    $('.js-furniture').click(function() {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;   
+    });
+});
